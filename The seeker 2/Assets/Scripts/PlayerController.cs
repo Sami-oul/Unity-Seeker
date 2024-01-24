799fbf81c7d682189172f3f4b66783a4fe15884c
Sami-oul <157427431+Sami-oul@users.noreply.github.com>
2024-01-24 22:38:57 +0100

    Commit #03
    Added Gravity
    Added some stairs

--- The seeker 2/Assets/Scripts/PlayerController.cs
+++ The seeker 2/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.Jobs;
 
@@ -11,9 +12,25 @@ public class PlayerController : MonoBehaviour
     public float smoothtime =  0.1f;
     float current;
 
+    public Transform checkTransform;
+    public float radius = .2f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    float Gravity;
+
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(checkTransform.position,radius,groundMask);
+        
+        if (isGrounded == false)
+        {
+            Gravity += -9.8f;
+            characterController.Move(new Vector3 (0,Gravity*Time.deltaTime*Time.deltaTime,0));
+        }else
+        {
+            Gravity = 0;
+        }
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
